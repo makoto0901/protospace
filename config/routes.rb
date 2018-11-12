8b8b7483df469495f17d41807e01fc9a5942b554
yoshikawaya <yoshikawakosuke@yoshikawakousukenoMacBook-puro.local>
2018-11-13 02:19:07 +0900

    プロトタイプの削除機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
-
+  delete 'prototypes/:id'  =>  'prototypes#destroy'
   resources :prototypes, only: [:index, :new, :create, :show]
   resources :users, only: [:show, :edit, :update]
 end
