97818afd470fb76d11b1479da27a9cc68fffcf35
takabatoshiki <takabatoshiki@gmail.com>
2018-11-18 11:18:47 +0900

    add preview_function to protottypes.js

---
+++ app/assets/javascripts/prototypes.js
@@ -0,0 +1,29 @@
+$(function(){
+  $('input[type="file"]').on('change', function(e){
+    var file = e.target.files[0];
+    if (!file.type.match('image.*')) {
+      alert("画像ファイルを指定してください。");
+      return;
+    }
+
+    var frameWidth = $(this).width();
+    var frameHeight = $(this).height();
+    var preview = $(this).prev('img')[0];
+    var reader = new FileReader();
+    reader.onload = function(evt){
+      preview.src = reader.result;
+      preview.width = frameWidth;
+      preview.height = frameHeight;
+      preview.onload = function(){
+        console.log("Load complete");
+      };
+    }
+
+    if (file) {
+      reader.readAsDataURL(file);
+    } else {
+      preview.src = "";
+    }
+
+  });
+});
