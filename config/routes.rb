1216a8f5e9a573d80145540de2d7d1bbf7288ec5
yoshikawaya <yoshikawakosuke@yoshikawakousukenoMacBook-puro.local>
2018-11-25 17:44:11 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
   delete 'prototypes/:id'  =>  'prototypes#destroy'
-  resources :prototypes, only: [:index, :new, :create, :show]
+  resources :prototypes, only: [:index, :new, :create, :show] do
+    resources :likes, only: [:create, :destroy]
+  end
   resources :users, only: [:show, :edit, :update]
 end
