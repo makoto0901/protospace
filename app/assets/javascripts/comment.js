7c2238f07a4510d5583225c53207e8b8887a9d9d
makoto <1988makoto0901@gmail.com>
2018-12-02 18:28:44 +0900

    es6を削除

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,30 +1,14 @@
 $(document).on("turbolinks:load",function(){
   var idx = 0;
   function buildHTML(comment){
-    let html =
-      `<div class='media'>
-        <div class='media-left'>
-          <a href=/users/${comment.user_id}>
-            <img alt='profile_photo' class='media-object' style='width: 64px; height: 64px;' src=${comment.avatar_url}>
-          </a>
-        </div>
-        <div class='media-body'>
-          <h4 class='media-heading' id='top-aligned-media'>
-            ${comment.user.name}
-            <a rel='nofollow' data-method='delete' href=/prototypes/${comment.prototype_id}/comments/${comment.id}>削除</a>
-            <a href=/prototypes/${comment.prototype_id}/comments/${comment.id}/edit>編集</a>
-          </h4>
-          <p>
-            ${comment.content}
-          </p>
-        </div>
-      </div>`
+    var html =
+      "<div class='media'><div class='media-left'><a href=/users/" + comment.user_id + "<img alt='profile_photo' class='media-object' style='width: 64px; height: 64px;' src=" +comment.avatar_url + "></a></div><div class='media-body'><h4 class='media-heading' id='top-aligned-media'>" + comment.user.name + "<a rel='nofollow' data-method='delete' href=/prototypes/" + comment.prototype_id + "/comments/" + comment.id + ">削除</a><a href=/prototypes/" + comment.prototype_id + "/comments/" + comment.id + "/edit>編集</a></h4><p>" + comment.content + "</p></div></div>"
     return html;
   }
   $('#new_comment').on('submit', function(e){
     e.preventDefault();
-    let formData = new FormData(this);
-    let url = $(this).attr('action');
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
       $.ajax({
         url: url,
         type: 'POST',
